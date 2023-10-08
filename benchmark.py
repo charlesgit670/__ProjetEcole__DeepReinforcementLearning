f370d7948ee886aaf8eed08dda7e127aadb47ee1
duincan <lopdun2000@gmail.com>
2023-10-09 00:00:51 +0200

    fini le jeu balloon pop, benchmark implemente, description et proposition des vecteurs encoding

--- benchmark.py
+++ benchmark.py
@@ -5,7 +5,6 @@ import time
 # Constants
 BALLOON_COLORS = ["Yellow", "Blue", "Red"]
 BALLOON_SHAPES = ["Star", "Moon", "Kite"]
-
 BALLOONS = BALLOON_COLORS + BALLOON_SHAPES
 
 BUST_LIMITS = {
@@ -37,82 +36,44 @@ BALLOON_SCORES = {
 NUM_DICE = 5
 NUM_BREAKS = 3
 
-def flatten(l):
-    return [item for sublist in l for item in sublist]
 def roll_dice(number_of_dice=NUM_DICE):
     """Roll the dice and return a list of balloon colors."""
-
-
     return [random.choice(list(DICE_FACE.values())) for i in range(number_of_dice)]
 
-
 def reroll_dice(dice, dice_to_reroll):
-
+    """Reroll specified dice."""
     nb_dice_to_reroll = len(dice_to_reroll) + 1
-
     reroll = roll_dice(nb_dice_to_reroll)
-
-    for index, new_result in (zip(dice_to_reroll,range(nb_dice_to_reroll))):
+    for index, new_result in (zip(dice_to_reroll, range(nb_dice_to_reroll))):
         dice[index] = reroll[new_result]
-
     dice[len(dice)] = reroll[-1]
-
     return dice
-
 
 def is_busted(balloons_collected):
     """Check if the player has busted."""
     for color, count in balloons_collected.items():
-
         if count >= BUST_LIMITS[color]:
-
-            print(f"Ooops! You busted! {color} exploded!")
-
             return True
-
     return False
 
+def auto_reroll_dice(dice):
+    """Automatically decide which dice to reroll for the bot."""
+    return reroll_dice(dice, [0])
 
-def play_turn():
-    """Play one turn of the game."""
+def auto_play_turn():
+    """Play one turn of the game automatically without user input."""
     balloons_collected = {color: 0 for color in BALLOONS}
     number_of_dice = 3
-
     dice = {}
-
-
     rolled = roll_dice(number_of_dice)
-
-
     for index in range(number_of_dice):
-
         dice[index] = rolled[index]
-    print(f"You rolled: {dice}")
-
-    while len(dice) < NUM_DICE:
-
-        reroll = input("Do you want to reroll? (y/n) ")
-        if reroll == "y":
-
-            dice_to_reroll = input("Which dice do you want to reroll? (e.g. 0 1 2 3 4 5) ")
-            dice_to_reroll = [int(i) for i in dice_to_reroll.split()]
-            dice = reroll_dice(dice, dice_to_reroll)
-            print(dice)
-
-
-        else:
-
-
-            break
-
+    dice = auto_reroll_dice(dice)
     c = Counter(x for xs in rolled for x in set(xs))
-
     return c
-
 
 def calculate_score(total_score, balloons_collected):
     """Calculate the score for the current turn."""
-
     for balloon, count in balloons_collected.items():
         total_score[balloon] += count
     return total_score
@@ -127,32 +88,27 @@ def count_score(total_score):
             scoring += 0
     return scoring
 
-
-
-def play_game():
-    """Play the Balloon Pop! game."""
+def auto_play_game():
+    """Automatically play the Balloon Pop! game without user input."""
     total_score = {color: 0 for color in BALLOONS}
     current_score = 0
-    print("Welcome to Balloon Pop!\n")
-    print("Your score is", current_score)
-
     for turn in range(NUM_BREAKS):
-
-        print(f"\n--- Turn {turn + 1} ---")
-
         while not is_busted(total_score):
-            print(f"Your accumulated balloons are {total_score}")
-            print(f"The ultimate bust limits are {BUST_LIMITS}")
-            balloons_collected = play_turn()
+            balloons_collected = auto_play_turn()
             total_score = calculate_score(total_score, balloons_collected)
-
-
-        print(f"Your accumulated balloons are {total_score}")
         current_score += count_score(total_score)
-        print(f"Your current accumulated score is {current_score}")
         total_score = {color: 0 for color in BALLOONS}
+    return current_score
 
+def benchmark():
+    """Benchmark the game to determine how many games can be played in one second."""
+    start_time = time.time()
+    count = 0
+    while time.time() - start_time < 1.0:
+        auto_play_game()
+        count += 1
+    return count
 
-
-# Play the game
-play_game()
+# Benchmark the game
+games_played_in_one_second = benchmark()
+print(games_played_in_one_second)
