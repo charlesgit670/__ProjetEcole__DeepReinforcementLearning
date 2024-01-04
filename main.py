38ec98ab44bc11e0a9488896c76c7120db44e941
clogeais <clogeais@meilleurtaux.com>
2024-01-04 14:58:23 +0100

    en cours de dev

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.DRL_algorithm.DQN import deep_q_learning
 from src.DRL_algorithm.DoubleDQN import double_deep_q_learning
 from src.DRL_algorithm.DoubleDQN_ExperienceReplay import double_deep_q_learning_with_experience_replay
 from src.DRL_algorithm.DoubleDQN_PrioritizedExperienceReplay import double_deep_q_learning_with_prioritized_experience_replay
+from src.DRL_algorithm.Reinforce import reinforce
 from src.agent_env.TicTacToeEnv import TicTacToeEnv
 
 env = TicTacToeEnv(True)
@@ -9,5 +10,6 @@ env = TicTacToeEnv(True)
 # deep_q_learning(env, max_episodes_count=100)
 # double_deep_q_learning(env, max_episodes_count=100)
 # double_deep_q_learning_with_experience_replay(env, max_episodes_count=1000)
-double_deep_q_learning_with_prioritized_experience_replay(env, max_episodes_count=1000)
+# double_deep_q_learning_with_prioritized_experience_replay(env, max_episodes_count=1000)
+reinforce(env, max_episodes_count=100)
 
--- src/DRL_algorithm/Reinforce.py
+++ src/DRL_algorithm/Reinforce.py
@@ -9,7 +9,23 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 
 from src.agent_env import SingleAgentEnv
+from src.DRL_algorithm.function_utils import acceptable_softmax_with_mask
+
 # https://medium.com/nerd-for-tech/reinforcement-learning-introduction-to-policy-gradients-aa2ff134c1b
+class CustomModel(tf.keras.Model):
+  def __init__(self, input_size, output_size):
+    super().__init__()
+    self.l1 = tf.keras.layers.Dense(64, input_shape=(input_size,), bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu')
+    self.l2 = tf.keras.layers.Dense(32, bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu')
+    self.out = tf.keras.layers.Dense(output_size, bias_initializer=tf.keras.initializers.RandomNormal(), activation='linear')
+
+  def call(self, input_data, mask):
+    x = tf.convert_to_tensor(input_data)
+    x = self.l1(x)
+    x = self.l2(x)
+    x = self.out(x)
+    x = acceptable_softmax_with_mask(x, mask)
+    return x
 
 def reinforce(env: SingleAgentEnv,
               gamma: float = 0.99999,
@@ -20,9 +36,9 @@ def reinforce(env: SingleAgentEnv,
     reward_episodes = []
 
     # init model
-    model = init_model(env.state_size, env.action_size, lr)
+    model = CustomModel(env.state_size, env.action_size)
+    # model = init_model(env.state_size, env.action_size, lr)
 
-    Returns = {}
     for ep_id in tqdm(range(max_episodes_count)):
         lenght_episode = 0
 
@@ -34,7 +50,10 @@ def reinforce(env: SingleAgentEnv,
             s = env.state_vector()
             mask = env.available_actions_mask()
 
-            a, lp = model.predict(s.reshape(1, len(s)), verbose=0)
+            # a, lp = model.predict(s.reshape(1, len(s)), verbose=0)
+            a = model(s.reshape(1, len(s)), mask.reshape(1, len(mask)))
+            assert(np.sum(a) == 1.0)
+            np.random.choice(a)
 
             old_score = env.score()
             env.act_with_action_id(a)
@@ -75,12 +94,28 @@ def reinforce(env: SingleAgentEnv,
         model.save(model_save_path)
 
 
-def init_model(input_size, output_size, lr):
-    model = Sequential([
-        Dense(units=64, input_shape=(input_size,), bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu'),
-        Dense(units=32, bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu'),
-        #Dense(units=output_size, bias_initializer=tf.keras.initializers.RandomNormal(), activation='linear')
-        # custom soft max
-    ])
-    model.compile(loss='mse', optimizer=Adam(learning_rate=lr))
-    return model
+class SoftmaxWithMask(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(SoftmaxWithMask, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(SoftmaxWithMask, self).build(input_shape)
+
+    def call(self, x, mask):
+        # Implémentation de la fonction softmax
+        # ans = acceptable_softmax_with_mask(x)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+# def init_model(input_size, output_size, lr):
+#     model = Sequential([
+#         Dense(units=64, input_shape=(input_size,), bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu'),
+#         Dense(units=32, bias_initializer=tf.keras.initializers.RandomNormal(), activation='relu'),
+#         Dense(units=output_size, bias_initializer=tf.keras.initializers.RandomNormal(), activation='linear'),
+#         SoftmaxWithMask()
+#     ])
+#     model.compile(loss='mse', optimizer=Adam(learning_rate=lr))
+#     return model
